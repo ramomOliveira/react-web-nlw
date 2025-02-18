8f1da2908894eff96104472a01cca4e69f4b3030
Ramom <ra-mom2000@hotmail.com>
2025-02-18 18:12:45 -0300

    separates home into a folder

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from '@/components/button'
-import { InputField, InputIcon, InputRoot } from '@/components/input'
-import { ArrowRight, Mail, Radio, User } from 'lucide-react'
+import { Radio } from 'lucide-react'
 import Image from 'next/image'
-import logo from '../assets/logo.svg'
+import logo from '../../assets/logo.svg'
+import { SubscriptionForm } from './subscription-form'
 export default function Home() {
   return (
-
     <div className="min-h-dvh flex flex-col justify-center gap-16">
       <div className="flex flex-col gap-8 items-center md:items-start">
         <Image src={logo} alt="Logo" width={108.5} height={30} />
@@ -38,32 +36,7 @@ export default function Home() {
           </p>
         </div>
 
-        <form className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]">
-          <h2 className="font-heading font-semibold text-gray-200 text-xl">
-            Inscrição
-          </h2>
-
-          <div className="space-y-3">
-            <InputRoot>
-              <InputIcon>
-                <User />
-              </InputIcon>
-              <InputField type="text" placeholder="Nome completo" />
-            </InputRoot>
-
-            <InputRoot>
-              <InputIcon>
-                <Mail />
-              </InputIcon>
-              <InputField type="email" placeholder="E-mail" />
-            </InputRoot>
-          </div>
-
-          <Button type='submit'>
-            Confirmar
-            <ArrowRight />
-          </Button>
-        </form>
+        <SubscriptionForm />
       </div>
     </div>
   )
