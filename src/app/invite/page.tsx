357e7c6a1856315e5a359b3ffb217229bd6ec7e3
Ramom <ra-mom2000@hotmail.com>
2025-02-18 19:44:30 -0300

    registration form with hook form

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -5,6 +5,8 @@ import { Ranking } from './ranking'
 import { Stats } from './stats'
 
 export default function InvitePage() {
+  const inviteLink = 'http://localhost:3000/3sdfsdf'
+
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
@@ -32,7 +34,7 @@ export default function InvitePage() {
             </p>
           </div>
 
-          <InviteLinkInput />
+          <InviteLinkInput inviteLink={inviteLink} />
 
           <Stats />
         </div>
