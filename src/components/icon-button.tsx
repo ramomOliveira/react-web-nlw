6811557b5e32d32601739a56b6bc49e73d0b32e2
Ramom <ra-mom2000@hotmail.com>
2025-02-18 01:48:49 -0300

    button icon

---
+++ src/components/icon-button.tsx
@@ -0,0 +1,16 @@
+import type { ComponentProps } from 'react'
+
+interface IconButtonProps extends ComponentProps<'button'> {
+  children: React.ReactNode
+}
+
+export function IconButton(props: IconButtonProps) {
+  return (
+    <button
+      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"
+      {...props}
+    >
+      {props.children}
+    </button>
+  )
+}
